51ed65c667a3a9cbfcd6447cb098ef253e27f54e
AKASH <88474098+AKASH-GO@users.noreply.github.com>
2023-11-28 10:45:09 +0530

    Music App

--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useSwiper } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
+import { Navigation } from "swiper";
+import styles from "./Carousel.module.scss";
 import "swiper/css";
+import CarouselLeftNavigation from "./CarouselLeftNavigation/CarouselLeftNavigation";
+import CarouselRightNavigation from "./CarouselRightNavigation/CarouselRIghtNavigation";
 
-function Carousel({data, renderComponent}){
-    
+const Controls = ({ data }) => {
+  const { swiper } = useSwiper();
+
+  useEffect(() => {
+    swiper.slideTo(0, 1);
+  }, [data, swiper]);
+
+  return <></>;
+};
+
+function Carousel({ data, renderComponent }) {
+  return (
+    <div className={styles.wrapper}>
+      <Swiper
+        style={{ padding: "0px 20px" }}
+        initialSlide={0}
+        modules={{ Navigation }}
+        slidesPerView={"auto"}
+        spaceBetween={40}
+        allowTouchMove
+      >
+        <Controls data={data} />
+        <CarouselLeftNavigation />
+        <CarouselRightNavigation />
+        
+        {data.map((ele) => (
+          <SwiperSlide>{renderComponent(ele)}</SwiperSlide>
+        ))}
+        
+      </Swiper>
+    </div>
+  );
 }
+
+export default Carousel;
